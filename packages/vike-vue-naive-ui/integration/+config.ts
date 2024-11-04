1ea26a0b5eadb0441c9ca4b332a13fda96351b54
jessiea <jessie.arce@bcm.edu>
2024-11-04 17:11:37 -0600

    JA=>Fix: Fix vike-vue config type import

--- packages/vike-vue-naive-ui/integration/+config.ts
+++ packages/vike-vue-naive-ui/integration/+config.ts
@@ -1,16 +1,17 @@
+export { config as default }
+
 import { type App } from 'vue'
-
 import type { Config } from 'vike/types'
-import type _ from 'vike-vue/config' // Needed for declaration merging of Config
+import type { config as _ } from 'vike-vue/config' // Needed for declaration merging of Config
 
-export default {
+const config = {
   name: 'vike-vue-naive-ui',
   require: {
     'vike-vue': '>=0.8.3',
   },
   onBeforeRenderHtml: 'import:vike-vue-naive-ui/__internal/integration/setupCssCollect:setupCssCollect',
   onAfterRenderHtml: 'import:vike-vue-naive-ui/__internal/integration/addCollectedCss:addCollectedCss',
-}
+} satisfies Config
 
 declare global {
   namespace Vike {
